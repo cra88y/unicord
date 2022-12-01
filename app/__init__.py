a22b59c19f687ba79cd7d124cd7a6211dbf2c59a
cra88y <cam@loudstyle.com>
2022-12-01 16:45:11 -0600

    updates for new host

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from .seeds import seed_commands
 from .models import db, User
 from .config import Config
 eventlet.monkey_patch()
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
 # Tell flask about our seed commands
 app.cli.add_command(seed_commands)
